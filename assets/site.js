f91bf788c523b2a2c49465401f3934f0e6067e69
Godfrey Best <godfreybest@Godfrey-Bests-MacBook.local>
2016-09-16 00:08:20 -0400

    hw finished

--- assets/site.js
+++ assets/site.js
@@ -1,12 +1,10 @@
 var gifPage = {
-	listOfAnimals: ["dog", "cat"],
+	listOfAnimals: ["dog", "cat", "rooster", "chicken", "anaconda"],
 	createButton: function(){
 		for(var i=0; i < this.listOfAnimals.length; i++){
 			// var animalDiv = $("<div>");
 			var animalButton = $("<button>");
-			console.log(this.listOfAnimals[i]);
 			animalButton.attr("data-name", this.listOfAnimals[i]);
-			console.log(animalButton);
 			animalButton.addClass("buttons");
 			animalButton.text(this.listOfAnimals[i]);
 			// animalDiv.append(animalButton);
@@ -31,8 +29,9 @@ var gifPage = {
 	                animalImage.attr("src", results[i].images.fixed_height_still.url);
 	                animalImage.addClass("gif");
 	                animalImage.attr("data-state", "still");
+	                animalImage.attr("data-still", results[i].images.fixed_height_still.url)
 	                animalImage.attr("data-animate", results[i].images.fixed_height.url);
-	                $(".gifArea").append(animalImage);
+	                $(".gifArea").prepend(animalImage);
 	            }
 			})
 		});
@@ -42,24 +41,29 @@ var gifPage = {
 
 		// This line of code will grab the input from the textbox
 		var newAnimal = $('#animal-input').val().trim();
-
+		
+		console.log(newAnimal);
 		// The movie from the textbox is then added to our array
-		this.listOfAnimals.push(newAnimal);
-		console.log(this.listOfAnimals);
+		gifPage.listOfAnimals.push(newAnimal);
+		console.log(gifPage.listOfAnimals);
+
 		// Our array then runs which handles the processing of our movie array
 		// this.createButton();
-		console.log(newAnimal);
+		$(".buttonArea").empty();
+		gifPage.createButton();
+		
 		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
 		return false;
 	});
 	},
 
 	stillVsAnimate: function(){
-		$('.gif').on('click', function(){
+		$(document).on('click', '.gif', function(){
 			var state = $(this).attr('data-state');
 			console.log(state);
 			if ( state == 'still'){
                 $(this).attr('src', $(this).data('animate'));
+                console.log($(this).data('animate'));
                 $(this).attr('data-state', 'animate');
             }else{
                 $(this).attr('src', $(this).data('still'));
